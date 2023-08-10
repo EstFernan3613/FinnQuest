8226974d891a21a8a15b362581fbb7d2999ced80
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-08-10 01:12:02 -0500

    Vida, Metodo Puntos y Enemigo Basico
    Falta enemigo mas complejo y Terminar Mapa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance {  get; private set; }
     public int PuntosTotales { get { return puntosTotales; } }
+    public HUD hud;
     private int puntosTotales;
+    private int vidas = 3;
 
     void Awake()
     {
@@ -23,5 +26,28 @@ public class GameManager : MonoBehaviour
     {
         puntosTotales += puntosASumar;
         Debug.Log(puntosTotales);
+        hud.ActualizarPuntos(PuntosTotales);
+    }
+
+    public void PerderVida()
+    {
+        vidas -= 1;
+        if(vidas == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+        hud.DesactivarVida(vidas);
+    }
+
+    public bool RecuperarVida()
+    {
+        if(vidas == 3)
+        {
+            return false;
+        }
+
+        hud.ActivarVida(vidas);
+        vidas += 1;
+        return true;
     }
 }
--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class HUD : MonoBehaviour
 {
     public TextMeshProUGUI puntos;
+    public GameObject[] vidas;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +18,20 @@ public class HUD : MonoBehaviour
     void Update()
     {
         puntos.text = GameManager.Instance.PuntosTotales.ToString();
+    }
+
+    public void ActualizarPuntos(int puntosTotales)
+    {
+        puntos.text = puntosTotales.ToString();
+    }
+
+    public void DesactivarVida(int indice)
+    {
+        vidas[indice].SetActive(false);
+    }
+
+    public void ActivarVida(int indice)
+    {
+        vidas[indice].SetActive(true);
     }
 }
